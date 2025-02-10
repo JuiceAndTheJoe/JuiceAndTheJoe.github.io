83a0eddb2a6547cad40f22fccbb40f09d4143a71
JuiceAndTheJoe <esvela02@gmail.com>
2025-02-10 19:13:54 +0100

    stylezz

--- web/web.js
+++ web/web.js
@@ -5,15 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
             card.classList.toggle('flipped');
             const cardText = this.querySelector('.card-front').textContent;
             let fortuneText = '';
+            let fortuneDesc = '';
             switch (cardText) {
-            case 'AOS':
+            case '1':
                 fortuneText = 'AOS';
+                fortuneDesc = 'A student organization connecting students, institutions, and the defense sector.';
                 break;
-            case 'On the Move with Daniel':
+            case '2':
                 fortuneText = 'On the Move with Daniel';
+                fortuneDesc = 'A fitness and personal development project with my uncle.';
                 break;
-            case 'Drawing App':
+            case '3':
                 fortuneText = 'Drawing App';
+                fortuneDesc = 'A simple drawing app using HTML5 Canvas, CSS and JS.';
                 break;
             default:
                 fortuneText = 'Choose your fortune...';
@@ -21,9 +25,11 @@ document.addEventListener('DOMContentLoaded', function() {
             const allFlippedDown = Array.from(document.querySelectorAll('.card')).every(card => !card.classList.contains('flipped'));
             if (allFlippedDown) {
                 fortuneText = 'Choose your fortune...';
+                fortuneDesc = '';
             }
 
             document.getElementById('fortune-text').textContent = fortuneText;
+            document.getElementById('fortune-desc').textContent = fortuneDesc;
         });
     });
 });
