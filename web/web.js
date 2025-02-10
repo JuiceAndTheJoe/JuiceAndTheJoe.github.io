5731fa1dba4a588b80365632c6774d404e6b84a5
JuiceAndTheJoe <esvela02@gmail.com>
2025-02-10 18:45:16 +0100

    chill like that

--- web/web.js
+++ web/web.js
@@ -3,6 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
     cards.forEach(card => {
         card.addEventListener('click', function() {
             card.classList.toggle('flipped');
+            const cardText = this.querySelector('.card-front').textContent;
+            let fortuneText = '';
+            switch (cardText) {
+            case 'AOS':
+                fortuneText = 'AOS';
+                break;
+            case 'On the Move with Daniel':
+                fortuneText = 'On the Move with Daniel';
+                break;
+            case 'Drawing App':
+                fortuneText = 'Drawing App';
+                break;
+            default:
+                fortuneText = 'Choose your fortune...';
+            }
+            const allFlippedDown = Array.from(document.querySelectorAll('.card')).every(card => !card.classList.contains('flipped'));
+            if (allFlippedDown) {
+                fortuneText = 'Choose your fortune...';
+            }
+
+            document.getElementById('fortune-text').textContent = fortuneText;
         });
     });
 });
